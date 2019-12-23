ef9e59b9c61e84f76d600f7eee49d9064c607cfd
Wenomr <wenom998@gmail.com>
2019-12-23 11:12:28 +0300

    Initial

---
+++ precache-manifest.96e5ccfd70720bdef49948747eb7fb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31a5b69eb70ff38b62229e9ed41330c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02caa0fa6c343786d445",
+    "url": "/static/css/main.cf7ee65e.chunk.css"
+  },
+  {
+    "revision": "314e1af11de7aa0bbb23",
+    "url": "/static/js/2.e71fb290.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e71fb290.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02caa0fa6c343786d445",
+    "url": "/static/js/main.053d6e9e.chunk.js"
+  },
+  {
+    "revision": "1302f09cd25884ff1395",
+    "url": "/static/js/runtime-main.74a7ecc7.js"
+  }
+]);
